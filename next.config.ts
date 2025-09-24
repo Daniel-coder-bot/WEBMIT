905a347de008729b0609bfa0d82d32d4492cbf64
Alan Perez <goldosh7@gmail.com>
2025-09-24 06:24:02 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,6 @@ import type {NextConfig} from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
-  basePath: '/WEBMIT',
-  assetPrefix: '/WEBMIT/',
   typescript: {
     ignoreBuildErrors: true,
   },
